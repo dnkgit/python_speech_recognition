8e76164fe12a75d6a2685acacc834f7a1d06bb0c
dnkgit <33461634+dnkgit@users.noreply.github.com>
2023-08-21 13:50:55 +0100

    Modified slightly to reflect which file has what text.

--- convert_speech.py
+++ convert_speech.py
@@ -32,14 +32,22 @@ def transcribe_audio(path):
 
 def get_large_audio_transcription_on_silence(path):
     sound = AudioSegment.from_file(path)
-    chunks = split_on_silence(sound, min_silence_len=500, silence_thresh= sound.dBFS-14,keep_silence=500)
+    MIN_SILENCE_LEN_MS = 1000           # The min length considered to be a pause
+    # SILENCE_THRESHOLD_DBFS = -40        # anything under -16 dBFS is considered to be silence
+    SILENCE_THRESHOLD_DBFS = sound.dBFS-14
+    KEEP_SILENCE_MS = 200               # keep this time of leading/trailing silence
+    chunks = split_on_silence(sound,
+                              min_silence_len = MIN_SILENCE_LEN_MS,
+                              silence_thresh = SILENCE_THRESHOLD_DBFS,
+                              keep_silence = KEEP_SILENCE_MS)
     folder_name = "audio-chunks"
     if not os.path.isdir(folder_name):
         os.mkdir(folder_name)
     whole_text = ''
     for i, audio_chunk in enumerate(chunks, start=1):
-        chunk_filename = os.path.join(folder_name, f"chunk{1}.wav")
+        chunk_filename = os.path.join(folder_name, f"chunk{i}.wav")
         audio_chunk.export(chunk_filename, format="wav")
+        print(f"Audio chunk {chunk_filename} :")
         try:
             text=transcribe_audio(chunk_filename)
         except sr.UnknownValueError as e:
@@ -47,14 +55,19 @@ def get_large_audio_transcription_on_silence(path):
         else:
             text = f"{text.capitalize()}. "
             print(chunk_filename, ":", text)
-            whole_text = whole_text + text
+            whole_text += text
     return whole_text
 
+doConvert = False
 # Do conversion if input is MP3
-src = "audio.mp3"
-dst = "audio.wav"
-conv_sound = AudioSegment.from_mp3(src)
-conv_sound.export(dst, format="wav")
-#path = r'c:\work\Test.wav'
+if doConvert:
+    src = "audio.mp3"
+    dst = "audio.wav"
+    conv_sound = AudioSegment.from_mp3(src)
+    conv_sound.export(dst, format="wav")
+else:
+    dst = r"c:\work\Test.wav"
+
 input = dst
+print(f"Using file [{dst}]")
 print("\nFull text :", get_large_audio_transcription_on_silence(input))
